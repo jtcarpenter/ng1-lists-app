230d5f78d016266d133cd4ccf9ff0d6005354930
jtcarpenter <jtcarpenter@gmail.com>
2013-05-29 12:48:48 +0000

    controllers.js edited online with Bitbucket

--- app/scripts/controllers/controllers.js
+++ app/scripts/controllers/controllers.js
@@ -3,6 +3,7 @@ listsApp.controller('ListsController', function(Lists, $scope) {
 
   $scope.addList = function() {
     var id = Lists.all.length;
+    // TODO: We don't want references to $scope inside our methods
     Lists.all.push({id: id, title: $scope.listTitle, items: []});
     $scope.listsMessage = 'New list \'' + $scope.listTitle + '\' added';
     $scope.listTitle = '';
