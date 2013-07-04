d5c1824076e34d2482100fbf5de1fc215015e6ff
Jason Carpenter <jtcarpenter@gmail.com>
2013-07-04 14:00:41 +0100

    Added a solution for displaying loading spinner.

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -2,7 +2,7 @@
 
 var app = angular.module('listsApp', ['listsApp.services', 'listsApp.directives']);
 
-app.config(['$routeProvider', function($routeProvider) {
+app.config(['$routeProvider', '$httpProvider', function($routeProvider, $httpProvider) {
   $routeProvider.
     when('/', {
       controller: 'ListsCtrl'
@@ -13,5 +13,24 @@ app.config(['$routeProvider', function($routeProvider) {
     }).
     otherwise({
       redirectTo: '/'
-    });
+  });
+
+  var spinner;
+  $httpProvider.defaults.headers.common['X-Blog-Version'] = '1.0'; // first thing we do when sending a request: pop a spinner 
+  $httpProvider.defaults.transformRequest.unshift(function (req) {
+    //spinner = angular.element('<h1>spinning...</h1>'); 
+    //console.log(document.body);
+    spinner = true;
+    console.log('spin');
+    return req; // return request unspoiled
+  });
+  // last thing we do when retrieving a response (before success() and the like): // hide the spinner.
+  $httpProvider.defaults.transformResponse.push(function (res) {
+    if (spinner) {
+      //spinner.remove();
+      spinner = false;
+      console.log('spin remove');
+    }
+    return res; // return response unspoiled 
+  });
 }]);
