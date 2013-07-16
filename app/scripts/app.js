10dee5f8388923cd3015e9c81a2870d362f68f7f
Jason Carpenter <jtcarpenter@gmail.com>
2013-07-16 13:49:16 +0100

    Added static spinner image.

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -16,16 +16,22 @@ app.config(['$routeProvider', '$httpProvider', function($routeProvider, $httpPro
   });
 
   var spinner;
+  var spinnerFrame;
   $httpProvider.defaults.headers.common['X-Blog-Version'] = '1.0'; // first thing we do when sending a request: pop a spinner 
   $httpProvider.defaults.transformRequest.unshift(function (req) {
     spinner = angular.element(document.getElementById('spinner'));
+    spinnerFrame = angular.element(document.getElementById('spinner-frame'));
     spinner.css('display', 'block');
+    spinnerFrame.css('display', 'none');
+    //spinner.removeClass('frame').addClass('spin');
     return req; // return request unspoiled
   });
   // last thing we do when retrieving a response (before success() and the like): // hide the spinner.
   $httpProvider.defaults.transformResponse.push(function (res) {
     if (spinner) {
       spinner.css('display', 'none');
+      spinnerFrame.css('display', 'block');
+      //spinner.removeClass('spin').addClass('frame');
     }
     return res; // return response unspoiled 
   });
